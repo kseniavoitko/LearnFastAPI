d1e8353063a6a88255a31e8c78499007ad491d62
kseniavoitko <kseniavoytko95@gmail.com>
2024-05-19 11:34:20 +0300

    hw12

---
+++ src/repository/users.py
@@ -0,0 +1,28 @@
+from sqlalchemy import select
+from libgravatar import Gravatar
+from sqlalchemy.orm import Session
+
+from src.database.models import User
+from src.schemas import UserModel
+
+
+async def get_user_by_email(email: str, db: Session) -> User | None:
+    stmt = select(User).filter_by(email=email)
+    user = await db.execute(stmt)
+    user = user.scalar_one_or_none()
+    return user
+
+
+async def create_user(body: UserModel, db: Session):
+    g = Gravatar(body.email)
+
+    new_user = User(**body.dict(), avatar=g.get_image())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+async def update_token(user: User, refresh_token, db: Session):
+    user.refresh_token = refresh_token
+    db.commit()
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -7,13 +7,18 @@ from sqlalchemy.orm import Session
 from src.database.db import get_db
 from src.schemas import UserModel, UserResponse, TokenModel
 from src.repository import users as repository_users
+from src.services.auth import auth_service
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def signup(body: UserModel, db: Session = Depends(get_db)):
-    pass
-    return {}
+    exist_user = await repository_users.get_user_by_email(body.email, db)
+    if exist_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
+    body.password = auth_service.get_password_hash(body.password)
+    new_user = await repository_users.create_user(body, db)
+    return new_user
 
 
 @router.post("/login", response_model=TokenModel)
