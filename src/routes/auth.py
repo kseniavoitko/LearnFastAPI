e7036123eadef440b8f16df5599eeded571f2754
kseniavoitko <kseniavoytko95@gmail.com>
2024-05-21 16:24:33 +0300

    hw12

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException, Security, status, Path, Query
-from fastapi.security import HTTPAuthorizationCredentials, OAuth2PasswordRequestForm
+from fastapi.security import HTTPAuthorizationCredentials, OAuth2PasswordRequestForm, HTTPBearer
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -10,6 +10,7 @@ from src.repository import users as repository_users
 from src.services.auth import auth_service
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+security = HTTPBearer()
 
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def signup(body: UserModel, db: Session = Depends(get_db)):
@@ -23,11 +24,28 @@ async def signup(body: UserModel, db: Session = Depends(get_db)):
 
 @router.post("/login", response_model=TokenModel)
 async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    pass
-    return {}
+    user = await repository_users.get_user_by_email(body.username, db)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
+    if not auth_service.verify_password(body.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
+    # Generate JWT
+    access_token = await auth_service.create_access_token(data={"sub": user.email})
+    refresh_token = await auth_service.create_refresh_token(data={"sub": user.email})
+    await repository_users.update_token(user, refresh_token, db)
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
 
 
 @router.get('/refresh_token', response_model=TokenModel)
-async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(), db: Session = Depends(get_db)):
-    pass
-    return {}
+async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_db)):
+    token = credentials.credentials
+    email = await auth_service.decode_refresh_token(token)
+    user = await repository_users.get_user_by_email(email, db)
+    if user.refresh_token != token:
+        await repository_users.update_token(user, None, db)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
+
+    access_token = await auth_service.create_access_token(data={"sub": email})
+    refresh_token = await auth_service.create_refresh_token(data={"sub": email})
+    await repository_users.update_token(user, refresh_token, db)
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -3,9 +3,11 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status, Path, Query
 from sqlalchemy.orm import Session
 
+from src.database.models import User
 from src.database.db import get_db
 from src.schemas import ContactModel, ResponseContact
 from src.repository import contacts as repository_contacts
+from src.services.auth import auth_service
 
 router = APIRouter(prefix="/contacts", tags=["contacts"])
 
@@ -18,13 +20,21 @@ async def get_contacts(
     lastname: str = "",
     email: str = "",
     birthdays: bool = False,
-    db: Session = Depends(get_db)):
-    contacts = await repository_contacts.get_contacts(limit, offset, firstname, lastname, email, birthdays, db)
+    db: Session = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
+):
+    contacts = await repository_contacts.get_contacts(
+        limit, offset, firstname, lastname, email, birthdays, db
+    )
     return contacts
 
 
 @router.get("/{contact_id}", response_model=ResponseContact)
-async def get_contact(contact_id: int, db: Session = Depends(get_db)):
+async def get_contact(
+    contact_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
+):
     contact = await repository_contacts.get_contact(contact_id, db)
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
@@ -32,14 +42,21 @@ async def get_contact(contact_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/", response_model=ResponseContact, status_code=status.HTTP_201_CREATED)
-async def create_contact(body: ContactModel, db: Session = Depends(get_db)):
+async def create_contact(
+    body: ContactModel,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
+):
     contact = await repository_contacts.create_contact(body, db)
     return contact
 
 
 @router.put("/{contact_id}", response_model=ResponseContact)
 async def update_contact(
-    body: ContactModel, contact_id: int, db: Session = Depends(get_db)
+    body: ContactModel,
+    contact_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
 ):
     contact = await repository_contacts.update_contact(body, contact_id, db)
     if contact is None:
@@ -49,7 +66,9 @@ async def update_contact(
 
 @router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def remove_contact(
-    contact_id: int, db: Session = Depends(get_db)
+    contact_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
 ):
     contact = await repository_contacts.remove_contact(contact_id, db)
     if contact is None:
